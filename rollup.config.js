09e675807cddcd526363bb7f002dd58376a753de
jaxonzhao <jaxonzhao@tencent.com>
2018-06-11 18:59:13 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ export default {
   input: './src/index.js',
   output: {
     file: 'dist/weapp-kit.js',
-    format: 'es'
-  },
-  intro: `/**
+    format: 'es',
+    intro: `/**
  * @author Zhao Jingchen
  * @license MIT
  * @link https://github.com/dracupid/weapp-kit
  */`
+  }
 }
